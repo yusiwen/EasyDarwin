aa3cd342d6f84e31233121688fe5fa2d489ab466
Siwen Yu <yusiwen@gmail.com>
2022-01-12 15:37:54 +0800

    init go mod
    Former-commit-id: 7441239db0b255fa1c5dd83375130702aceb3483 [formerly 7441239db0b255fa1c5dd83375130702aceb3483 [formerly 7441239db0b255fa1c5dd83375130702aceb3483 [formerly 53e9ea3785a6523e474b7f5793fc47e8da04907c]]]
    Former-commit-id: cdac36bd469606a4258c236ea7712d975eb47f52
    Former-commit-id: 67a3ea9f939d6b79cbebf1b5b9d7af211c1f9e9f [formerly acd8d4465785328bce5359bf96f4fba734255ff0]
    Former-commit-id: 0ea528ba379663367690352f3247a8e5ab09ca5d

--- rtsp/rtsp-session.go
+++ rtsp/rtsp-session.go
@@ -18,8 +18,8 @@ import (
 	"time"
 
 	"github.com/EasyDarwin/EasyDarwin/models"
-	"github.com/penggy/EasyGoLib/db"
-	"github.com/penggy/EasyGoLib/utils"
+	"github.com/MeloQi/EasyGoLib/db"
+	"github.com/MeloQi/EasyGoLib/utils"
 
 	"github.com/teris-io/shortid"
 )
